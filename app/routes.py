2ac4954c9da245dfc2840be1dbcd077256d12877
Danielli Hiraoka <daniellihiraoka@gmail.com>
2024-09-04 14:59:16 -0300

    nova pasta app definindo as rotas login e registro

---
+++ app/routes.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify, session
+from app import app, db
+from app.models import User
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    data = request.json
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+    if User.query.filter_by(email=email).first():
+        return jsonify({'message': 'Email já cadastrado!'}), 400
+
+    new_user = User(username=username, email=email)
+    new_user.set_password(password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'Usuário registrado com sucesso!'})
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
+
+    user = User.query.filter_by(email=email).first()
+
+    if user is None or not user.check_password(password):
+        return jsonify({'message': 'Credenciais inválidas!'}), 401
+
+    # Sessão do usuário
+    session['user_id'] = user.id
+    return jsonify({'message': 'Login realizado com sucesso!'})
