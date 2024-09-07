8643727dcb1e9dd845633639591b605d4f842410
Danielli Hiraoka <daniellihiraoka@gmail.com>
2024-09-07 18:25:19 -0300

    secret_key

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+app.config['SECRET_KEY'] = 'a1b2c3d4e5f6g7h8i9j0k1234567890abcdefg'
 
 # Configurando a URI de conexão ao PostgreSQL
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:impacta@localhost/db_impacta2'
