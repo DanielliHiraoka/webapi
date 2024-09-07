b1af27e6e08cfbf3b018cfb281c81e9d4dcd8a1c
Danielli Hiraoka <daniellihiraoka@gmail.com>
2024-09-07 20:11:55 -0300

    atualização feddback login e registro

--- app/routes.py
+++ app/routes.py
@@ -13,15 +13,22 @@ def register():
     email = data.get('email')
     password = data.get('password')
 
-    if User.query.filter_by(email=email).first():
-        return jsonify({'message': 'Email já cadastrado!'}), 400
+    # Verificar se o nome de usuário já existe
+    if User.query.filter_by(username=username).first():
+        return jsonify({'message': 'Nome de usuário já cadastrado!'}), 400
 
+    # Verificar se o e-mail já existe
+    if User.query.filter_by(email=email).first():
+        return jsonify({'message': 'E-mail já cadastrado!'}), 400
+
+    # Criar um novo usuário
     new_user = User(username=username, email=email)
     new_user.set_password(password)
     db.session.add(new_user)
     db.session.commit()
 
     return jsonify({'message': 'Usuário registrado com sucesso!'})
+
 
 @app.route('/api/login', methods=['POST'])
 def login():
@@ -32,8 +39,7 @@ def login():
     user = User.query.filter_by(email=email).first()
 
     if user is None or not user.check_password(password):
-        return jsonify({'message': 'Credenciais inválidas!'}), 401
+        return jsonify({'message': 'Email ou Senha inválidas!'}), 401
 
-    # Sessão do usuário
     session['user_id'] = user.id
     return jsonify({'message': 'Login realizado com sucesso!'})
